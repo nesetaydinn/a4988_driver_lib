3fc92ee731f9eaf67ec1973058ba1bb9f85d6450
nesetaydinn <nesataydin1669@gmail.com>
2022-03-09 11:16:54 +0300

    Update motor_pins type name motor_pins -> motor_pins_t & Add motor_ms_pins_t type & Add motor_state_t type & Add motor_pins_t type

--- a4988/type_definations.h
+++ a4988/type_definations.h
@@ -2,10 +2,34 @@
 #define TYPE_DEFINATIONS_H_
 
 #include "main.h"
+#include "stdint.h"
+
+#include "enum_definations.h"
+
+
+typedef uint8_t motor_speed_t;
 
 typedef struct{
 	GPIO_TypeDef * port;
 	uint16_t pin;
-}motor_pins;
+}motor_pins_t;
+
+typedef struct{
+	motor_pins_t step_pins[0x03];
+}motor_ms_pins_t;
+
+typedef struct{
+	microstep_resolition microstepping_state;
+	motor_direction direction_state;
+	motor_speed_t motor_speed;
+}motor_state_t;
+
+typedef struct{
+	TIM_HandleTypeDef * htim;
+	motor_ms_pins_t microstepping_pins;
+	motor_pins_t dir_pin;
+	motor_state_t driver_state;
+}motor_pins_t;
+
 
 #endif
