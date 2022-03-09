5a236c7456e2b74b202f4251d58a4eeaa01ab27c
nesetaydinn <nesataydin1669@gmail.com>
2022-03-09 14:47:24 +0300

    Update timer freq calculator

--- a4988/a4988.c
+++ a4988/a4988.c
@@ -3,15 +3,24 @@
 a4988_t a4988DrvInit(TIM_HandleTypeDef * htim, motor_pins_t * dir_pin){
 	a4988_t tmp;
 	tmp.dir_pin = dir_pin;
-/*
+
 	TIM_ClockConfigTypeDef sClockSourceConfig = {0};
 	TIM_MasterConfigTypeDef sMasterConfig = {0};
 	TIM_OC_InitTypeDef sConfigOC = {0};
 	TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+	uint16_t f_psc = 0, f_arr = 255;
+	uint32_t f_apb1 = HAL_RCC_GetPCLK1Freq();
+	uint32_t f_apb2 = HAL_RCC_GetPCLK2Freq();
+	if ((htim->Instance == (TIM_TypeDef *)(STM32_TIM_1)) || (htim->Instance == (TIM_TypeDef *)(STM32_TIM_8))  ||
+		(htim->Instance == (TIM_TypeDef *)(STM32_TIM_9)) || (htim->Instance == (TIM_TypeDef *)(STM32_TIM_10)) || 
+		(htim->Instance == (TIM_TypeDef *)(STM32_TIM_11)))
+		f_psc = (f_apb2 / (1000 * (f_arr + 1))) - 1;
+	else 
+		f_psc = (f_apb1 / (1000 * (f_arr + 1))) - 1;
 
-	htim->Init.Prescaler = 217;
+	htim->Init.Prescaler = f_psc;
 	htim->Init.CounterMode = TIM_COUNTERMODE_UP;
-	htim->Init.Period = 255;
+	htim->Init.Period = f_arr;
 	htim->Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
 	htim->Init.RepetitionCounter = 0;
 	htim->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -57,7 +66,7 @@ a4988_t a4988DrvInit(TIM_HandleTypeDef * htim, motor_pins_t * dir_pin){
 		Error_Handler();
 	}
 
-	HAL_TIM_MspPostInit(htim);*/
+	HAL_TIM_MspPostInit(htim);
 	tmp.htim = htim;
 	tmp.pins_check.is_set_dir_pin = true;
 	tmp.pins_check.is_set_enable_pin = false;
